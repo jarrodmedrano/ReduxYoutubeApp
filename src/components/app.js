9d171c8d63ab0c92446b4e921f7c236ebed1aa8f
Jarrod Medrano <jmedran@gmail.com>
2016-02-16 16:37:28 -0800

    adding youtube api

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Component } from 'react';
 
+const API_KEY = 'AIzaSyDmfK88i5f08cvkS5wL08uNkNuj9OaHBbg';
+
 export default class App extends Component {
   render() {
     return (
