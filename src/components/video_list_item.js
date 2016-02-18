1cc2cf54f1644d392f50e125d90951a7232fcf67
jmedran <jmedran@gmail.com>
2016-02-17 20:36:35 -0800

    adding list group

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  ///const video = props.video; same as above
   return(
-    <li>
-      Video
+    <li className="list-group-item">
+      <div className="video-list-media">
+        <div className="media-left">
+          <img className="media-object" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+          </div>
+        </div>
+      </div>
     </li>
   );
 };
